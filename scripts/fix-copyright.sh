96da7bc2ceec0391bfb34e61a74730b14a929ece
Theron Voran <tvoran@users.noreply.github.com>
2023-01-04 21:23:06 -0800

    Adding CRT build workflow and make targets (#8)
    Co-authored-by: Ben Ash <32777270+benashz@users.noreply.github.com>

---
+++ scripts/fix-copyright.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+set -e
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="${SCRIPT_DIR%/*}"
+
+for y in $(find ${DIR}/config/crd/bases ${DIR}/config/rbac/role.yaml -name "*.yaml"); do
+    OUTFILE=$(mktemp)
+
+    cat ${DIR}/hack/boilerplate.yaml.txt <(echo) $y > $OUTFILE
+    mv $OUTFILE $y
+done
