32a2efb425a6bed79746cbb05ba2c3cdf02436dd
Kyle Schochenmaier <kschoche@gmail.com>
2023-02-07 11:45:02 -0800

    remove unnecessary function

--- test/unit/_helpers.bash
+++ test/unit/_helpers.bash
@@ -5,11 +5,3 @@
 chart_dir() {
     echo ${BATS_TEST_DIRNAME}/../../chart/
 }
-
-# Usage: assert_empty helm template -s <template> [flags] .
-# assert_empty makes it possible to test that a template is not rendered.
-assert_empty() {
-    run helm "${@:2}"
-    [ "$status" -eq 1 ]
-    [[ "$output" =~ "Error: could not find template" ]]
-}
