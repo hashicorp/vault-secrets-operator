57761feb41cc878a0a566050566caa3af244715c
Ben Ash <bash@hashicorp.com>
2023-03-02 09:53:16 -0500

    Add the demo Terraform and related code
    Can be run with:
    
    make -f demo.mk demo

---
+++ demo/README.md
@@ -0,0 +1,17 @@
+## Vault Secrets Operator Demo (Dynamic Secrets)
+
+Brings up a kind cluster running Vault, the Vault Secrets Operator, Postgres, and a simple Deployment application.
+
+To run the demo, perform the following from the project root
+
+If you haven't already built the vault-secrets-operator Docker image run:
+```shell
+make  docker-build
+```
+
+Deploy the demo app:
+```shell
+make -f demo.mk demo
+```
+
+You can poke around on the demo cluster using one of your favorite tools.
---
+++ demo/infra/app/variables.tf
@@ -0,0 +1,34 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+variable "operator_namespace" {
+  default = "vault-secrets-operator-system"
+}
+
+variable "k8s_config_context" {
+  default = "kind-vault-secrets-operator"
+}
+
+variable "k8s_config_path" {
+  default = "~/.kube/config"
+}
+
+variable "k8s_host" {
+  default = "https://kubernetes.default.svc"
+}
+
+variable "postgres_secret_name" {
+  default = "postgres-postgresql"
+}
+
+variable "vault_enterprise" {
+  type    = bool
+  default = false
+}
+
+variable "k8s_db_secret_count" {
+  default = 50
+}
+
+variable "vault_address" {}
+variable "vault_token" {}
