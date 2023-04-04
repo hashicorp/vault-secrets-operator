572a51b0ead0ef54b8666d6ba12e69c2d3dd8742
Adrian Moisey <adrian@changeover.za.net>
2023-04-04 08:38:58 +0200

    Update hack/install_copywrite.sh
    Co-authored-by: Kyle Schochenmaier <kyle.schochenmaier@hashicorp.com>

--- hack/install_copywrite.sh
+++ hack/install_copywrite.sh
@@ -41,7 +41,7 @@ pushd $tempdir > /dev/null
 popd > /dev/null
 
 rm -f "${dest_file%/*}"/copywrite*
-mkdir bin/
+mkdir -p bin/
 mv "${tempdir}/copywrite" "${dest_file}"
 ln -sf "${dest_filename}" "${dest_file%/*}/copywrite"
 
