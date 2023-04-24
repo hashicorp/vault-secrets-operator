54fc8cda476199122c6c7b34e1b34049ba4275a2
Kyle Schochenmaier <kschoche@gmail.com>
2023-04-24 12:28:13 -0500

    fix lint

--- test/integration/vaultauthmethods/terraform/variables.tf
+++ test/integration/vaultauthmethods/terraform/variables.tf
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: MPL-2.0
 
 variable "k8s_test_namespace" {
-  type = string
+  type    = string
   default = "testing"
 }
 
@@ -11,27 +11,27 @@ variable "k8s_vault_connection_address" {
 }
 
 variable "k8s_config_context" {
-  type = string
+  type    = string
   default = "kind-kind"
 }
 
 variable "k8s_config_path" {
-  type = string
+  type    = string
   default = "~/.kube/config"
 }
 
 variable "k8s_host" {
-  type = string
+  type    = string
   default = "https://kubernetes.default.svc"
 }
 
 variable "vault_kvv2_mount_path" {
-  type = string
+  type    = string
   default = "kvv2"
 }
 
 variable "vault_test_namespace" {
-  type = string
+  type    = string
   default = "tenant-1"
 }
 
@@ -42,17 +42,17 @@ variable "vault_enterprise" {
 
 # The path to the local helm chart in our repository, this is used by helm to find the Chart.yaml
 variable "operator_helm_chart_path" {
-  type = string
+  type    = string
   default = "../../../../chart"
 }
 
 variable "operator_namespace" {
-  type = string
+  type    = string
   default = "vault-secrets-operator-system"
 }
 
 # AppRole specific variables
 variable "approle_role_name" {
-  type = string
+  type    = string
   default = "approle"
 }
