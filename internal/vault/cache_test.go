3eda52191f9431f5af1d24e9db8f58f22e99a372
Thy Ton <maithytonn@gmail.com>
2023-05-26 10:29:43 -0700

    refactor test struct

--- internal/vault/cache_test.go
+++ internal/vault/cache_test.go
@@ -14,39 +14,38 @@ import (
 )
 
 func Test_clientCache_Prune(t *testing.T) {
-	var (
-		dummyCallbackFunc = func(key, value interface{}) {}
-		size              = 256
-		cacheLen          = 1
-		err               error
-		ctrl              = gomock.NewController(t)
-	)
+	dummyCallbackFunc := func(key, value interface{}) {}
+	ctrl := gomock.NewController(t)
 
 	tests := []struct {
 		name                  string
 		filterFuncReturnsTrue bool
-		expectedCacheLen      int
+		cacheSize             int
+		cacheLen              int
 	}{
 		{
 			name:                  "filterFunc returns true",
 			filterFuncReturnsTrue: true,
-			expectedCacheLen:      0,
+			cacheSize:             256,
+			cacheLen:              1,
 		},
 		{
 			name:                  "filterFunc returns false",
 			filterFuncReturnsTrue: false,
-			expectedCacheLen:      cacheLen,
+			cacheSize:             256,
+			cacheLen:              1,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			c := &clientCache{}
-			c.cache, err = lru.NewWithEvict(size, dummyCallbackFunc)
+			cache, err := lru.NewWithEvict(tt.cacheSize, dummyCallbackFunc)
+			c.cache = cache
 			require.NoError(t, err)
 
 			var expectedKeys []ClientCacheKey
-			for i := 0; i < cacheLen; i++ {
+			for i := 0; i < tt.cacheLen; i++ {
 				mockClient := NewMockClient(ctrl)
 				key := ClientCacheKey(fmt.Sprintf("key%d", i))
 				if tt.filterFuncReturnsTrue {
@@ -57,12 +56,12 @@ func Test_clientCache_Prune(t *testing.T) {
 				}
 				c.cache.Add(key, mockClient)
 			}
-			assert.Equal(t, cacheLen, c.cache.Len(), "unexpected cache len before calling Prune()")
+			assert.Equal(t, tt.cacheLen, c.cache.Len(), "unexpected cache len before calling Prune()")
 			keys := c.Prune(func(Client) bool {
 				return tt.filterFuncReturnsTrue
 			})
 			assert.EqualValues(t, expectedKeys, keys)
-			assert.Equal(t, tt.expectedCacheLen, c.cache.Len())
+			assert.Equal(t, tt.cacheLen-len(expectedKeys), c.cache.Len())
 		})
 	}
 }
