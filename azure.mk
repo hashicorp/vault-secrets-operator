7e532aad11ba4ed2aac92d65f7335a0c0a841121
Gautam <gautambaghel93@gmail.com>
2023-05-31 13:40:22 -0700

    Testing VSO in Azure K8s Service (AKS) (#218)
    * adding files for azure/aks
    
    * adding oidc url

---
+++ azure.mk
@@ -0,0 +1,50 @@
+# run the following from the project root:
+# make -f azure.mk <make rule>
+
+# Azure variables cloud hosted k8s testing
+AZURE_REGION ?= westus2
+AKS_K8S_VERSION ?= 1.25.6
+ACR_REPO_PREFIX ?= vso
+
+# directories for cloud hosted k8s infrastructure for running tests
+TF_AKS_SRC_DIR ?= $(INTEGRATION_TEST_ROOT)/infra/aks
+TF_AKS_STATE_DIR ?= $(TF_AKS_SRC_DIR)/state
+
+include ./Makefile
+
+##@ AKS
+
+.PHONY: create-aks
+create-aks: ## Create a new AKS cluster
+	@mkdir -p $(TF_AKS_STATE_DIR)
+	cp -v $(TF_AKS_SRC_DIR)/*.tf $(TF_AKS_STATE_DIR)/.
+	$(TERRAFORM) -chdir=$(TF_AKS_STATE_DIR) init -upgrade
+	$(TERRAFORM) -chdir=$(TF_AKS_STATE_DIR) apply -auto-approve \
+		-var region=$(AZURE_REGION) \
+		-var kubernetes_version=$(AKS_K8S_VERSION) \
+		-var container_repository_prefix=$(ACR_REPO_PREFIX) || exit 1 \
+	rm -f $(TF_AKS_STATE_DIR)/*.tfvars
+
+.PHONY: import-azure-vars
+import-azure-vars:
+-include $(TF_AKS_STATE_DIR)/outputs.env
+
+# Currently only supports amd64
+.PHONY: build-push
+build-push: import-azure-vars ci-build ci-docker-build ## Build the operator image and push it to the ACR repository
+	az acr login --name $(ACR_NAME)
+	docker push $(IMG)
+
+.PHONY: integration-test-aks
+integration-test-aks: build-push ## Run integration tests in the AKS cluster
+	az aks get-credentials --admin --name $(AKS_CLUSTER_NAME) --overwrite-existing --resource-group $(AZURE_RSG_NAME)
+	$(MAKE) port-forward &
+	$(MAKE) integration-test K8S_CLUSTER_CONTEXT=$(K8S_CLUSTER_CONTEXT) IMAGE_TAG_BASE=$(IMAGE_TAG_BASE) \
+	IMG=$(IMG) VAULT_OIDC_DISC_URL=$(AKS_OIDC_URL) VAULT_OIDC_CA=false
+
+.PHONY: destroy-aks
+destroy-aks: ## Destroy the AKS cluster
+	$(TERRAFORM) -chdir=$(TF_AKS_STATE_DIR) destroy -auto-approve \
+		-var region=$(AZURE_REGION) \
+		-var kubernetes_version=$(AKS_K8S_VERSION) \
+		-var container_repository_prefix=$(ACR_REPO_PREFIX) || exit 1
---
+++ test/integration/infra/aks/variables.tf
@@ -0,0 +1,25 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+variable "region" {
+  default     = "westus2"
+  description = "Azure region to deploy the resources"
+}
+
+variable "kubernetes_version" {
+  description = "Kubernetes version for the AKS cluster"
+  type        = string
+  default     = "1.25.6"
+}
+
+variable "container_repository_prefix" {
+  description = "The Azure container repo prefix (.azurecr.io) for storing the operator image"
+  type        = string
+  default     = "vso"
+}
+
+variable "image_tag_base" {
+  description = "The name for the operator image"
+  type        = string
+  default     = "vault-secrets-operator"
+}
