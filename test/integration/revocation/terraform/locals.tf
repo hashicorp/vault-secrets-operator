596c294d1473603059a88bc67cf092dbacf89cd1
Thy Ton <maithytonn@gmail.com>
2023-08-10 19:45:20 -0700

    reformat terraform

--- test/integration/revocation/terraform/locals.tf
+++ test/integration/revocation/terraform/locals.tf
@@ -4,5 +4,5 @@
 locals {
   name_prefix = "${var.name_prefix}-${random_string.prefix.result}"
   policy_name = "${local.name_prefix}-dev"
-  namespace = var.vault_enterprise ? vault_namespace.test[0].path_fq : null
+  namespace   = var.vault_enterprise ? vault_namespace.test[0].path_fq : null
 }
