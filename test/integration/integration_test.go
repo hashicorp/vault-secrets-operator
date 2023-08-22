8459895c6cf7a7775b8ec14f8dd7a334ef822fa3
Thy Ton <maithytonn@gmail.com>
2023-08-21 23:39:22 -0700

    optionally revoke and purge all cached vault clients upon Helm uninstall (#202)
    ---------
    
    Co-authored-by: Ben Ash <bash@hashicorp.com>
    Co-authored-by: Kyle Schochenmaier <kschoche@gmail.com>

--- test/integration/integration_test.go
+++ test/integration/integration_test.go
@@ -293,6 +293,11 @@ func checkTLSFields(secret *corev1.Secret) (ok bool, err error) {
 	return true, nil
 }
 
+type revocationK8sOutputs struct {
+	AuthRole   string `json:"auth_role"`
+	PolicyName string `json:"policy_name"`
+}
+
 type authMethodsK8sOutputs struct {
 	AuthRole      string `json:"auth_role"`
 	AppRoleRoleID string `json:"role_id"`
---
+++ test/integration/revocation/terraform/variables.tf
@@ -0,0 +1,60 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: BUSL-1.1
+
+
+
+variable "k8s_test_namespace" {
+  default = "testing"
+}
+
+variable "k8s_vault_connection_address" {}
+
+variable "k8s_config_context" {
+  default = "kind-kind"
+}
+
+variable "k8s_config_path" {
+  default = "~/.kube/config"
+}
+
+variable "k8s_host" {
+  default = "https://kubernetes.default.svc"
+}
+
+variable "vault_kvv2_mount_path" {
+  default = "kvv2"
+}
+
+variable "vault_test_namespace" {
+  default = "tenant-1"
+}
+
+variable "vault_enterprise" {
+  type    = bool
+  default = false
+}
+
+# The path to the local helm chart in our repository, this is used by helm to find the Chart.yaml
+variable "operator_helm_chart_path" {
+  default = "../../../../chart"
+}
+
+variable "operator_namespace" {
+  default = "vault-secrets-operator-system"
+}
+
+variable "operator_image_repo" {
+  default = "hashicorp/vault-secrets-operator"
+}
+
+variable "operator_image_tag" {
+  default = "0.0.0-dev"
+}
+
+variable "auth_role" {
+  default = "role1"
+}
+
+variable "name_prefix" {
+  default = ""
+}
