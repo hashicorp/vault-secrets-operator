d60c502e362a4d70bb22130d88ceb541afce04e3
Ben Ash <32777270+benashz@users.noreply.github.com>
2023-09-08 16:46:31 -0400

    Apply suggestions from code review
    Co-authored-by: Theron Voran <tvoran@users.noreply.github.com>

--- api/v1beta1/hcpauth_types.go
+++ api/v1beta1/hcpauth_types.go
@@ -16,11 +16,11 @@ type HCPAuthSpec struct {
 	// AllowedNamespaces Kubernetes Namespaces which are allow-listed for use with this AuthMethod.
 	// This field allows administrators to customize which Kubernetes namespaces are authorized to
 	// use with this AuthMethod. While Vault will still enforce its own rules, this has the added
-	// configurability of restricting which VaultAuthMethods can be used by which namespaces.
+	// configurability of restricting which HCPAuthMethods can be used by which namespaces.
 	// Accepted values:
 	// []{"*"} - wildcard, all namespaces.
 	// []{"a", "b"} - list of namespaces.
-	// unset - disallow all namespaces except the Operator's the VaultAuthMethod's namespace, this
+	// unset - disallow all namespaces except the Operator's the HCPAuthMethod's namespace, this
 	// is the default behavior.
 	AllowedNamespaces []string `json:"allowedNamespaces,omitempty"`
 	// Method to use when authenticating to Vault.
