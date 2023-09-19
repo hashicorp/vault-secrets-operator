39be4556f218421787b24669dde03975057fcff6
Ben Ash <bash@hashicorp.com>
2023-09-19 13:06:08 -0400

    Check rollout-restart replica state

--- test/integration/integration_test.go
+++ test/integration/integration_test.go
@@ -517,7 +517,13 @@ func assertRolloutRestarts(
 ) error {
 	t.Helper()
 
+	type status struct {
+		Replicas      int32
+		ReadyReplicas int32
+	}
 	var errs error
+	var s *status
+
 	// see secretsv1beta1.RolloutRestartTarget for supported target resources.
 	timeNow := time.Now().UTC()
 	for _, target := range targets {
@@ -534,18 +540,21 @@ func assertRolloutRestarts(
 				annotations = o.Spec.Template.Annotations
 				tObj = &o
 			}
+			s = &status{o.Status.Replicas, o.Status.ReadyReplicas}
 		case "StatefulSet":
 			var o appsv1.StatefulSet
 			if assert.NoError(t, client.Get(ctx, tObjKey, &o)) {
 				annotations = o.Spec.Template.Annotations
 				tObj = &o
 			}
+			s = &status{o.Status.Replicas, o.Status.ReadyReplicas}
 		case "ReplicaSet":
 			var o appsv1.ReplicaSet
 			if assert.NoError(t, client.Get(ctx, tObjKey, &o)) {
 				annotations = o.Spec.Template.Annotations
 				tObj = &o
 			}
+			s = &status{o.Status.Replicas, o.Status.ReadyReplicas}
 		default:
 			assert.Fail(t,
 				"fatal, unsupported rollout-restart Kind %q for target %v", target.Kind, target)
@@ -572,6 +581,9 @@ func assertRolloutRestarts(
 		}
 		assert.True(t, ts.Before(timeNow),
 			"timestamp value %q for %q is in the future, now=%q", ts, expectedAnnotation, timeNow)
+		if s.ReadyReplicas != s.Replicas {
+			errs = errors.Join(errs, fmt.Errorf("expected ready replicas %d, actual %d", s.Replicas, s.ReadyReplicas))
+		}
 	}
 	return errs
 }
