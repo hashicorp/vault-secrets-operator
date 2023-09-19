78b6d068b5857c6c71e3383f1efcf43c46d0c79e
Ben Ash <32777270+benashz@users.noreply.github.com>
2023-09-19 09:10:05 -0400

    Add integration tests for HCPVaultSecretsApp (#357)
    Additional fixes:
    - add support for test TF modules. First module is for deploying VSO using Helm. We can adapt the other tests to use it later.

---
+++ test/integration/modules/vso-helm/variables.tf
@@ -0,0 +1,40 @@
+# Copyright (c) HashiCorp, Inck
+# SPDX-License-Identifier: BUSL-1.1
+variable "k8s_vault_connection_address" {
+  default = ""
+}
+
+# The path to the local helm chart in our repository, this is used by helm to find the Chart.yaml
+variable "operator_helm_chart_path" {
+  default = "../../../../chart"
+}
+
+variable "operator_namespace" {
+  default = "vault-secrets-operator-system"
+}
+
+variable "operator_image_repo" {
+  default = "hashicorp/vault-secrets-operator"
+}
+
+variable "operator_image_tag" {
+  default = "0.0.0-dev"
+}
+
+variable "k8s_auth_default_token_audiences" {
+  default = ""
+}
+
+variable "k8s_auth_default_role" {
+  default = ""
+}
+
+variable "enable_default_connection" {
+  type    = bool
+  default = true
+}
+
+variable "enable_default_auth_method" {
+  type    = bool
+  default = true
+}
