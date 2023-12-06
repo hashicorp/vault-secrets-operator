7c9cd80a49fd8d5c1c59a41e2bb327affca49f16
Ben Ash <bash@hashicorp.com>
2023-12-06 17:56:53 -0500

    Make bats run faster
    drop use of the yq docker image, replacing it with a binary from the
    project repo.

--- hack/set_containerImage.sh
+++ hack/set_containerImage.sh
@@ -1,6 +1,8 @@
-#!/usr/bin/env bash -e
+#!/usr/bin/env bash
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: BUSL-1.1
+#
+set -e -o pipefail
 
 # Set the annotation `containerImage` in
 # bundle/manifests/vault-secrets-operator.clusterserviceversion.yaml to the
@@ -9,7 +11,8 @@
 BUILD_DIR="${BUILD_DIR:-build}"
 HACK_DIR=$(dirname "$0")
 CSV_FILE=${HACK_DIR}/../${BUILD_DIR}/bundle/manifests/vault-secrets-operator.clusterserviceversion.yaml
-YQ=${HACK_DIR}/../scripts/yq
+# bin/yq is installed by running 'make yq'
+YQ=${HACK_DIR}/../bin/yq
 
 IMAGE=$(cat ${CSV_FILE} | ${YQ} '.spec.install.spec.deployments.[] | select(.name == "vault-secrets-operator-controller-manager") | .spec.template.spec.containers.[] | select(.name == "manager") | .image')
 
