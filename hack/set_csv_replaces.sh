9965b33517b6546b2e091f2e06bc616d119e253f
Theron Voran <tvoran@users.noreply.github.com>
2024-02-09 10:21:22 -0800

    make set_csv_replaces.sh more robust
    And add missing version field in csv customresourcedefinitions.

--- hack/set_csv_replaces.sh
+++ hack/set_csv_replaces.sh
@@ -11,17 +11,23 @@ set -e -o pipefail
 #
 # Override the `replaces` version by setting PREVIOUS_VERSION to "vX.Y.Z"
 
-PREVIOUS_GIT_TAG=$(git describe --abbrev=0 --tags "$(git rev-list --tags --skip=1 --max-count=1)")
-PREVIOUS_VERSION="${PREVIOUS_VERSION:-${PREVIOUS_GIT_TAG}}"
+if [ -z "${PREVIOUS_VERSION}" ]; then
+  PREVIOUS_GIT_TAG=$(git describe --abbrev=0 --tags "$(git rev-list --tags --skip=1 --max-count=1)")
+  PREVIOUS_VERSION="${PREVIOUS_GIT_TAG}"
+fi
+
 CSV="build/bundle/manifests/vault-secrets-operator.clusterserviceversion.yaml"
 
-CHECK="$(grep 'replaces:' ${CSV})"
+CHECK="$(grep 'replaces:' ${CSV} || true)"
 if [ -n "${CHECK}" ]; then
   echo "replaces already set in ${CSV}:"
   echo "${CHECK}"
   exit 0
 fi
 
-{
-  echo "  replaces: vault-secrets-operator.${PREVIOUS_VERSION}"
-} >> ${CSV}
+if [ -z "${PREVIOUS_VERSION}" ]; then
+  echo "unable to determine PREVIOUS_VERSION"
+  exit 1
+fi
+
+echo "  replaces: vault-secrets-operator.${PREVIOUS_VERSION?}" >> ${CSV}
