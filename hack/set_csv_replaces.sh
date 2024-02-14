efcc67f41f62bf65244138303b6d3e70041ce1f4
Theron Voran <tvoran@users.noreply.github.com>
2024-02-13 23:17:49 -0800

    Set OLM bundle to "Seamless Upgrades" (#581)
    Also sets the replaces parameter in ClusterServiceVersion to the n-1
    tag during make bundle, and fills in the description a bit more from
    the repo readme.
    
    ---------
    
    Co-authored-by: Ben Ash <32777270+benashz@users.noreply.github.com>

---
+++ hack/set_csv_replaces.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: BUSL-1.1
+set -e -o pipefail
+
+# Set the `spec.replaces` parameter in the ClusterServiceVersion (CSV) to the
+# previous version, so that the graph of CSVs is built correctly and previous
+# versions show up in OperatorHub.
+#
+# https://access.redhat.com/documentation/en-us/openshift_container_platform/4.2/html/operators/understanding-the-operator-lifecycle-manager-olm
+#
+# Override the `replaces` version by setting PREVIOUS_VERSION to "vX.Y.Z"
+
+if [ -z "${PREVIOUS_VERSION}" ]; then
+  PREVIOUS_GIT_TAG=$(git describe --abbrev=0 --tags "$(git rev-list --tags --skip=1 --max-count=1)")
+  PREVIOUS_VERSION="${PREVIOUS_GIT_TAG}"
+fi
+
+CSV="build/bundle/manifests/vault-secrets-operator.clusterserviceversion.yaml"
+
+CHECK="$(grep 'replaces:' ${CSV} || true)"
+if [ -n "${CHECK}" ]; then
+  echo "replaces already set in ${CSV}:"
+  echo "${CHECK}"
+  exit 0
+fi
+
+if [ -z "${PREVIOUS_VERSION}" ]; then
+  echo "unable to determine PREVIOUS_VERSION"
+  exit 1
+fi
+
+echo "  replaces: vault-secrets-operator.${PREVIOUS_VERSION?}" >> ${CSV}
