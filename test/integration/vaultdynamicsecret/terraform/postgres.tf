462929bb97dc652ad61a9971b8c1d8a1c72706a8
Ben Ash <bash@hashicorp.com>
2024-04-19 15:50:13 -0400

    postgres init: exit with a non-zero status on failure

--- test/integration/vaultdynamicsecret/terraform/postgres.tf
+++ test/integration/vaultdynamicsecret/terraform/postgres.tf
@@ -53,10 +53,11 @@ until [ $tries -ge 60 ]
 do
   kubectl exec -n ${self.triggers.namespace} ${self.triggers.pod} -- \
   psql postgresql://postgres:${self.triggers.password}@127.0.0.1:5432/postgres \
-  -c 'CREATE ROLE "${local.db_role_static_user}"' && break
+  -c 'CREATE ROLE "${local.db_role_static_user}"' && exit 0
   ((++tries))
   sleep .5
 done
+exit 1
 EOT
   }
 }
