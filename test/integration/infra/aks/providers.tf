dfb31912e34c68bc45a0930b73df52dd42594b37
Ben Ash <bash@hashicorp.com>
2024-05-10 14:29:53 +0000

    Bump TF provider versions

--- test/integration/infra/aks/providers.tf
+++ test/integration/infra/aks/providers.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.91.0"
+      version = "3.103.1"
     }
   }
 
--- test/integration/infra/eks/providers.tf
+++ test/integration/infra/eks/providers.tf
@@ -6,12 +6,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.36.0"
+      version = "5.49.0"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "3.6.0"
+      version = "3.6.1"
     }
   }
 
