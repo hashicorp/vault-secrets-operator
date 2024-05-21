e9004ada192dd5d81b3352f275122fec8c5e4a26
Ben Ash <bash@hashicorp.com>
2024-05-21 10:52:00 -0400

    Core: lock on get backoff

--- controllers/registry.go
+++ controllers/registry.go
@@ -251,8 +251,8 @@ func (r *BackOffRegistry) Delete(objKey client.ObjectKey) bool {
 // If objKey is not in the set of registered objects, it will be added. Return
 // true if the sync backoff entry was created.
 func (r *BackOffRegistry) Get(objKey client.ObjectKey) (*BackOff, bool) {
-	r.mu.RLock()
-	defer r.mu.RUnlock()
+	r.mu.Lock()
+	defer r.mu.Unlock()
 
 	entry, ok := r.m[objKey]
 	if !ok {
