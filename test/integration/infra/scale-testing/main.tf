f4abcbba0dacb063ee178ecd57c77477ad226ab4
Zlaticanin <zlaticaninmilena@gmail.com>
2024-08-19 12:16:08 -0700

    update

--- test/integration/infra/scale-testing/main.tf
+++ test/integration/infra/scale-testing/main.tf
@@ -125,7 +125,7 @@ module "vso-helm" {
 
 module "vault" {
   source               = "../../../modules/vault"
-  //vault_license_path   = var.vault_license_path
+  vault_license_path   = var.vault_license_path
   k8s_namespace        = var.k8s_namespace
   k8s_service_account  = var.k8s_service_account
   k8s_config_context   = var.k8s_config_context
