d8faecfc96e50e57cd9ff132dddd89942d8c7388
Thy Ton <maithytonn@gmail.com>
2024-08-28 14:56:55 -0700

    add OPERATOR_* env vars to output file terraform scale-testing

--- scale-testing.mk
+++ scale-testing.mk
@@ -44,19 +44,28 @@ endif
 import-aws-vars:
 -include $(TF_EKS_STATE_DIR)/outputs.env
 
-.PHONY: scale-test
-scale-test: import-aws-vars
+.PHONY: import-deploy-vars
+import-deploy-vars:
+-include $(TF_DEPLOY_STATE_DIR)/outputs.env
+
+.PHONY: connect-cluster
+connect-cluster: import-aws-vars ## Connect to the EKS cluster
 	aws eks --region $(AWS_REGION) update-kubeconfig --name $(EKS_CLUSTER_NAME)
+
+.PHONY: scale-test
+scale-test: connect-cluster import-aws-vars import-deploy-vars
 	$(MAKE) port-forward &
 	SCALE_TESTS=true \
-	SUPPRESS_TF_OUTPUT=$(SUPPRESS_TF_OUTPUT) SKIP_CLEANUP=$(SKIP_CLEANUP) OPERATOR_NAMESPACE=$(OPERATOR_NAMESPACE) \
+	SUPPRESS_TF_OUTPUT=$(SUPPRESS_TF_OUTPUT) SKIP_CLEANUP=$(SKIP_CLEANUP) \
+	OPERATOR_IMAGE_REPO=$(OPERATOR_IMAGE_REPO) OPERATOR_IMAGE_TAG=$(OPERATOR_IMAGE_TAG) \
+	OPERATOR_NAMESPACE=$(OPERATOR_NAMESPACE) \
 	VAULT_OIDC_DISC_URL=$(EKS_OIDC_URL) VAULT_OIDC_CA=false \
 	INTEGRATION_TESTS=true EKS_CLUSTER_NAME=$(EKS_CLUSTER_NAME) K8S_CLUSTER_CONTEXT=$(K8S_CLUSTER_CONTEXT) \
 	K8S_VAULT_NAMESPACE=$(K8S_VAULT_NAMESPACE) \
 	SKIP_AWS_TESTS=$(SKIP_AWS_TESTS) SKIP_AWS_STATIC_CREDS_TEST=$(SKIP_AWS_STATIC_CREDS_TEST) \
 	SKIP_GCP_TESTS=$(SKIP_GCP_TESTS) \
 	PARALLEL_INT_TESTS=$(INTEGRATION_TESTS_PARALLEL) \
-	go test github.com/hashicorp/vault-secrets-operator/test/integration/... -test.v -test.run TestVaultStaticSecret -timeout=30m
+	go test github.com/hashicorp/vault-secrets-operator/test/integration/... -test.v -test.run TestVaultStaticSecret/existing -timeout=30m
 
 .PHONY: destroy-eks
 destroy-eks: ## Destroy the EKS cluster
