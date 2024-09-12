0c93f97869879a8f6c10865de9eb2a3e5cf46399
Thy Ton <maithytonn@gmail.com>
2024-09-12 12:36:15 -0700

    add modules/operator-common

--- test/integration/operator/terraform/outputs.tf
+++ test/integration/operator/terraform/outputs.tf
@@ -2,23 +2,29 @@
 # SPDX-License-Identifier: BUSL-1.1
 
 output "k8s_namespace" {
-  value = local.k8s_namespace
+  value = module.operator_common.k8s_namespace
 }
+
 output "auth_role_operator" {
-  value = local.auth_role_operator
+  value = module.operator_common.auth_role_operator
 }
+
 output "transit_ref" {
-  value = one(kubernetes_manifest.vault-auth-operator[*].manifest.metadata.name)
+  value = module.operator_common.transit_ref
 }
+
 output "transit_path" {
-  value = vault_mount.transit.path
+  value = module.operator_common.transit_path
 }
+
 output "transit_key_name" {
-  value = vault_transit_secret_backend_key.cache.name
+  value = module.operator_common.transit_key_name
 }
+
 output "k8s_config_context" {
-  value = var.k8s_config_context
+  value = module.operator_common.k8s_config_context
 }
+
 output "namespace" {
-  value = local.operator_namespace
+  value = module.operator_common.namespace
 }
