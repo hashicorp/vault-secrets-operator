c6b93c4df39b63960dfb9b32899243e340d08fda
Milena Zlaticanin <60530402+Zlaticanin@users.noreply.github.com>
2025-05-02 11:10:58 -0700

    Add scale tests (#916)
    * scale testing
    
    * add OPERATOR_* env vars to output file terraform scale-testing
    
    * update makefile
    
    * update
    
    * wip: dropped remoted state and tests passing
    
    * add data sources to be able to get cluster outputs
    
    * revert change and remove unnecessary code
    
    * add modules/operator-common
    
    * add a make target to cleanup orphan port forward processes + refactor operator-scale to use the module
    
    * update cleanup-port-forward and add a flag to skip HCPVSAPPS tests
    
    * Add support to specify the number of CRs by type: VDS + VSS count
    
    * Extend vps to be able to specify count via env var
    
    * update cr names
    
    * delete operator-scale and use aliases for providers instead
    
    * make fmttf
    
    * add default for cluster name
    
    * add aws provider
    
    * Add aws region
    
    * add aws region
    
    * update aws region var
    
    * factor out providers files and use correct in testmain based on test case
    
    * make fmttf
    
    * move providers files into their own dir + add aws provider in our vault deployment
    
    * fix the providers
    
    * rename providers
    
    * update
    
    * update destination
    
    * update providers
    
    * update with suggestions
    
    * Update counts so we set default if count is <= 0
    
    * update default counts + add conditional for postgres storage persistence
    
    * update with suggestions
    
    * make fmttf
    
    * Use latest version of VSO in scale test
    
    * commit
    
    ---------
    
    Co-authored-by: Thy Ton <maithytonn@gmail.com>
    Co-authored-by: Val Conklin <val@hashicorp.com>

--- scale-testing.mk
+++ scale-testing.mk
@@ -5,6 +5,11 @@
 AWS_REGION ?= us-east-2
 EKS_K8S_VERSION ?= 1.30
 
+# testing dev instances is currently not supported
+# TODO: create the docker registry (e.g. ECR) to enable dev builds
+VERSION ?= 0.10.0
+INTEGRATION_TESTS_PARALLEL ?= true
+
 # directories for cloud hosted k8s infrastructure for running tests
 # root directory for all integration tests
 TF_EKS_SRC_DIR ?= $(INTEGRATION_TEST_ROOT)/infra/scale-testing/eks-cluster
@@ -12,21 +17,12 @@ TF_EKS_STATE_DIR ?= $(TF_EKS_SRC_DIR)/state
 TF_DEPLOY_SRC_DIR ?= $(INTEGRATION_TEST_ROOT)/infra/scale-testing/deployments
 TF_DEPLOY_STATE_DIR ?= $(TF_DEPLOY_SRC_DIR)/state
 
-include ./Makefile
+SCALE_TESTS ?= 1
 
-.PHONY: create-eks
-create-eks: ## Create a new EKS cluster
-	@mkdir -p $(TF_EKS_STATE_DIR)
-	rm -f $(TF_EKS_STATE_DIR)/*.tf
-	cp -v $(TF_EKS_SRC_DIR)/*.tf $(TF_EKS_STATE_DIR)/.
-	$(TERRAFORM) -chdir=$(TF_EKS_STATE_DIR) init -upgrade
-	$(TERRAFORM) -chdir=$(TF_EKS_STATE_DIR) apply -auto-approve \
-		-var region=$(AWS_REGION) \
-		-var kubernetes_version=$(EKS_K8S_VERSION) || exit 1
-	rm -f $(TF_EKS_STATE_DIR)/*.tfvars
+include ./aws.mk
 
 .PHONY: deploy-workload
-deploy-workload: set-vault-license ## Deploy the workload to the EKS cluster
+deploy-workload: set-vault-license import-aws-vars ## Deploy the workload to the EKS cluster
 	@mkdir -p $(TF_DEPLOY_STATE_DIR)
 ifeq ($(VAULT_ENTERPRISE), true)
     ## ensure that the license is *not* emitted to the console
@@ -35,11 +31,33 @@ endif
 	rm -f $(TF_DEPLOY_STATE_DIR)/*.tf
 	cp -v $(TF_DEPLOY_SRC_DIR)/*.tf $(TF_DEPLOY_STATE_DIR)/.
 	$(TERRAFORM) -chdir=$(TF_DEPLOY_STATE_DIR) init -upgrade
-	$(TERRAFORM) -chdir=$(TF_DEPLOY_STATE_DIR) apply -auto-approve || exit 1
+	$(TERRAFORM) -chdir=$(TF_DEPLOY_STATE_DIR) apply -auto-approve \
+		-var cluster_name=$(EKS_CLUSTER_NAME) || exit 1
 	rm -f $(TF_DEPLOY_STATE_DIR)/*.tfvars
 
-.PHONY: destroy-eks
-destroy-eks: ## Destroy the EKS cluster
-	$(TERRAFORM) -chdir=$(TF_EKS_STATE_DIR) destroy -auto-approve \
-		-var region=$(AWS_REGION) \
-		-var kubernetes_version=$(EKS_K8S_VERSION) || exit 1
+.PHONY: update-kubeconfig
+update-kubeconfig: import-aws-vars
+	aws eks --region $(AWS_REGION) update-kubeconfig --name $(EKS_CLUSTER_NAME)
+
+.PHONY: cleanup-port-forward
+cleanup-port-forward: ## Kill orphan port-forward processes
+	@echo "Cleaning up orphan port-forward processes..."
+	@pgrep -f 'kubectl port-forward -n $(K8S_VAULT_NAMESPACE) statefulset/vault' | xargs -r kill -9 && \
+		echo "Port-forward processes terminated successfully." || \
+		echo "No port-forward processes found or an error occurred."
+
+.PHONY: set image scale-tests ## Run integration tests on the EKS cluster
+scale-tests: cleanup-port-forward set-image update-kubeconfig import-aws-vars
+	$(MAKE) port-forward &
+	SCALE_TESTS=true VAULT_ENTERPRISE=true ENT_TESTS=$(VAULT_ENTERPRISE) \
+	SUPPRESS_TF_OUTPUT=$(SUPPRESS_TF_OUTPUT) SKIP_CLEANUP=$(SKIP_CLEANUP) \
+	OPERATOR_IMAGE_REPO=$(IMAGE_TAG_BASE) OPERATOR_IMAGE_TAG=$(VERSION) \
+	OPERATOR_NAMESPACE=$(OPERATOR_NAMESPACE) \
+	VAULT_OIDC_DISC_URL=$(EKS_OIDC_URL) VAULT_OIDC_CA=false \
+	INTEGRATION_TESTS=true EKS_CLUSTER_NAME=$(EKS_CLUSTER_NAME) \
+	K8S_CLUSTER_CONTEXT=$(K8S_CLUSTER_CONTEXT) CGO_ENABLED=0 \
+	K8S_VAULT_NAMESPACE=$(K8S_VAULT_NAMESPACE) \
+	SKIP_AWS_TESTS=$(SKIP_AWS_TESTS) SKIP_AWS_STATIC_CREDS_TEST=$(SKIP_AWS_STATIC_CREDS_TEST) \
+	SKIP_GCP_TESTS=$(SKIP_GCP_TESTS) SKIP_HCPVSAPPS_TESTS=$(SKIP_HCPVSAPPS_TESTS) \
+	PARALLEL_INT_TESTS=$(INTEGRATION_TESTS_PARALLEL) \
+	go test github.com/hashicorp/vault-secrets-operator/test/integration/... $(TESTARGS) -timeout=30m
--- test/integration/infra/scale-testing/deployments/variables.tf
+++ test/integration/infra/scale-testing/deployments/variables.tf
@@ -1,116 +1,9 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: BUSL-1.1
 
-variable "operator_namespace" {
-  default = "vault-secrets-operator-system"
-}
-
-# The path to the local helm chart in our repository, this is used by helm to find the Chart.yaml
-variable "operator_helm_chart_path" {
-  default = "../../../../../../chart"
-}
-
-variable "enable_default_connection" {
-  type    = bool
-  default = true
-}
-
-variable "enable_default_auth_method" {
-  type    = bool
-  default = true
-}
-
-variable "k8s_vault_connection_address" {
-  default = ""
-}
-
-variable "k8s_auth_default_mount" {
-  default = ""
-}
-
-variable "vault_test_namespace" {
-  default = ""
-}
-
-variable "operator_allowednamespaces" {
-  type    = list(string)
-  default = []
-}
-
-variable "k8s_auth_default_role" {
-  default = ""
-}
-
-variable "k8s_auth_default_token_audiences" {
-  type    = list(string)
-  default = []
-}
-
-variable "operator_image_repo" {
-  default = "hashicorp/vault-secrets-operator"
-}
-
-variable "operator_image_tag" {
-  default = "0.8.1"
-}
-
-variable "cpu_limits" {
-  default = ""
-}
-
-variable "memory_limits" {
-  default = ""
-}
-
-variable "cpu_requests" {
-  default = ""
-}
-
-variable "memory_requests" {
-  default = ""
-}
-
-variable "client_cache_config" {
-  type = object({
-    persistence_model                = string
-    revoke_client_cache_on_uninstall = bool
-    storage_encryption = object({
-      enabled                         = bool
-      vault_connection_ref            = string
-      namespace                       = string
-      mount                           = string
-      transit_mount                   = string
-      key_name                        = string
-      method                          = string
-      kubernetes_auth_role            = string
-      kubernetes_auth_service_account = string
-      kubernetes_auth_token_audiences = string
-    })
-  })
-
-  default = {
-    persistence_model                = ""
-    revoke_client_cache_on_uninstall = false
-    storage_encryption = {
-      enabled                         = false
-      vault_connection_ref            = ""
-      namespace                       = ""
-      mount                           = ""
-      transit_mount                   = ""
-      key_name                        = ""
-      method                          = ""
-      kubernetes_auth_role            = ""
-      kubernetes_auth_service_account = ""
-      kubernetes_auth_token_audiences = ""
-    }
-  }
-}
-
-variable "manager_extra_args" {
-  type = list(string)
-  default = [
-    "-zap-log-level=5"
-  ]
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
 }
 
 variable "vault_license_path" {
@@ -137,16 +30,8 @@ variable "k8s_config_path" {
   default = "~/.kube/config"
 }
 
-variable "vault_image_repo" {
-  default = "docker.mirror.hashicorp.services/hashicorp/vault"
-}
-
 variable "vault_image_repo_ent" {
   default = "docker.mirror.hashicorp.services/hashicorp/vault-enterprise"
-}
-
-variable "vault_image_tag" {
-  default = "1.17"
 }
 
 variable "vault_image_tag_ent" {
@@ -162,12 +47,8 @@ variable "vault_chart_version" {
   default = "0.29.1"
 }
 
-variable "install_kube_prometheus" {
-  type    = bool
-  default = false
-}
-
-variable "metrics_server_enabled" {
-  type    = bool
-  default = true
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-2"
 }
