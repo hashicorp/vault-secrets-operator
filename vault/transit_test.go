d2ff0e546af2cc4ae902558b049fd2e58a8e6a2e
Michael Blaum <michael.blaum@hashicorp.com>
2025-11-24 18:12:21 -0500

    add test

---
+++ vault/transit_test.go
@@ -0,0 +1,32 @@
+package vault
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestWithKeyVersion(t *testing.T) {
+	tests := []struct {
+		name string
+		v    int
+	}{
+		{"key version 1", 1},
+		{"key version 42", 42},
+		{"zero value", 0},
+		{"negative", -5},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			opts := WithKeyVersion(tt.v)
+			m := make(map[string]any)
+
+			opts(m)
+
+			require.Contains(t, m, "key_version")
+			assert.Equal(t, tt.v, m["key_version"])
+		})
+	}
+}
