072a952095d631086822ee2f6c74dce96c9337c6
Michael Blaum <96261585+hashiblaum@users.noreply.github.com>
2025-11-25 13:56:22 -0500

    Add Transit Options to Encrypt/Decrypt Helpers (#1156)
    * add opts to transit, in order to add request params
    
    * comments
    
    * add test
    
    * update transit signatures
    
    * update test
    
    * update test

--- vault/transit.go
+++ vault/transit.go
@@ -21,16 +21,26 @@ type (
 	decryptResponse struct {
 		Plaintext string `json:"plaintext"`
 	}
+
+	// TransitOption modifies parameters for a Transit Encrypt/Decrypt request.
+	// Individual options may only apply to certain operations.
+	TransitOption func(m map[string]any)
 )
 
 // EncryptWithTransit encrypts data using Vault Transit.
-func EncryptWithTransit(ctx context.Context, vaultClient Client, mount, key string, data []byte) ([]byte, error) {
+func EncryptWithTransit(ctx context.Context, vaultClient Client, mount, key string, data []byte, opts ...TransitOption) ([]byte, error) {
 	path := fmt.Sprintf("%s/encrypt/%s", mount, key)
-	resp, err := vaultClient.Write(ctx, NewWriteRequest(path, map[string]any{
+
+	params := map[string]any{
 		"name":      key,
 		"plaintext": base64.StdEncoding.EncodeToString(data),
-	}, nil),
-	)
+	}
+
+	for _, opt := range opts {
+		opt(params)
+	}
+
+	resp, err := vaultClient.Write(ctx, NewWriteRequest(path, params, nil))
 	if err != nil {
 		return nil, err
 	}
@@ -75,4 +85,10 @@ func DecryptWithTransit(ctx context.Context, vaultClient Client, mount, key stri
 	}
 
 	return base64.StdEncoding.DecodeString(d.Plaintext)
+}
+
+// WithKeyVersion sets the key version for EncryptWithTransit.
+// It is ignored when passed to DecryptWithTransit.
+func WithKeyVersion(v uint) TransitOption {
+	return func(m map[string]any) { m["key_version"] = v }
 }
